0fdc134936d4a1ed231b5f1ded4e27eb34c0486e
Ben Daniels <benjamindaniels21@gmail.com>
2023-11-22 20:22:17 -0500

    adding more validation

--- app.js
+++ app.js
@@ -48,6 +48,9 @@ app.post(
       campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
       }).required(),
     });
 
