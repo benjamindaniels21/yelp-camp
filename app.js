f8117a04d76ab74949b873804862b8a028d07b97
Ben Daniels <benjamindaniels21@gmail.com>
2023-11-28 11:31:29 -0500

    setting up delete route for a campground

--- app.js
+++ app.js
@@ -110,6 +110,13 @@ app.post(
   })
 );
 
+app.delete(
+  "/campgrounds/:id/reviews/:reviewId",
+  catchAsync(async (req, res) => {
+    res.send("delete me!");
+  })
+);
+
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page not found!", 404));
 });
