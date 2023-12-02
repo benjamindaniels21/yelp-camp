dede6dacbf96f4f752a127eafc4122504acf794c
Ben Daniels <benjamindaniels21@gmail.com>
2023-12-02 16:47:51 -0500

    cleanup

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError");
-const Joi = require("joi");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
