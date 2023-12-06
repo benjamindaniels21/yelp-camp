b8644e85a0dcda5e8b0f28e8d04b67b8734b0df6
Ben Daniels <benjamindaniels21@gmail.com>
2023-12-06 17:05:45 -0500

    updates

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -41,6 +44,10 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize);
+app.use(passport.session);
+passport.use(new localStrategy(User.authenticate()));
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
