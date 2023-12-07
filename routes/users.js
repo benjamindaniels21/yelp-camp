024929c43be5d8dd5fcc1d554e0ca67829655f76
Ben Daniels <benjamindaniels21@gmail.com>
2023-12-07 13:25:40 -0500

    checking for uniqueness in register

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  const { email, username, password } = req.body;
-  const user = new User({ email, username });
-  const registeredUser = await User.register(user, password);
-  console.log(registeredUser);
-  req.flash("success", "Welcome to Yelp Camp!");
-  res.redirect("/campgrounds");
-});
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("register");
+    }
+  })
+);
 
 module.exports = router;
