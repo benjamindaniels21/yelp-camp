38cbcfbd10b41a57e9a9287038982b7c39289c13
Ben Daniels <benjamindaniels21@gmail.com>
2023-12-07 18:30:03 -0500

    check authentication for new route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,6 +25,10 @@ router.get(
 );
 
 router.get("/new", (req, res) => {
+  if (!req.isAuthenticated()) {
+    req.flash("error", "You must be signed in!");
+    res.redirect("/login");
+  }
   res.render("campgrounds/new");
 });
 
