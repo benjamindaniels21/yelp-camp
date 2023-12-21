ee364eb1e6be35fddf5e79f0650421b07ea97d94
Ben Daniels <benjamindaniels21@gmail.com>
2023-12-21 08:26:39 -0500

    adding photos from cloud to seeds

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,20 @@ const seedDB = async () => {
       author: "65735a200be862c745b4dec1",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: `https://source.unsplash.com/collection/483251`,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dsprnjo2q/image/upload/v1703164799/YelpCamp/um3ylhhpraahyw5slqmr.jpg",
+          filename: "YelpCamp/um3ylhhpraahyw5slqmr",
+        },
+        {
+          url: "https://res.cloudinary.com/dsprnjo2q/image/upload/v1703164882/YelpCamp/fywvaem8sgpioayqitdw.jpg",
+          filename: "YelpCamp/fywvaem8sgpioayqitdw",
+        },
+        {
+          url: "https://res.cloudinary.com/dsprnjo2q/image/upload/v1703164926/YelpCamp/irtrpznmg92ncrwut77s.jpg",
+          filename: "YelpCamp/irtrpznmg92ncrwut77s",
+        },
+      ],
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tempore velit vel dignissimos reprehenderit quam, ipsum eligendi quos eum molestias, eius at, aliquid excepturi fugit animi nobis voluptatum laudantium fuga.`,
       price: price,
     });
