edf860b92cd4eb21e42c8c1b774dd3e339b2fd10
Ben Daniels <benjamindaniels21@gmail.com>
2023-12-23 20:30:46 -0500

    fix adding images on edit page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,6 +11,8 @@ const multer = require("multer");
 const { storage } = require("../cloudinary");
 const upload = multer({ storage });
 
+const Campground = require("../models/campground");
+
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
