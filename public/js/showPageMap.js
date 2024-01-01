0faff5f1126cf78f27a5989be64f76b048bec878
Ben Daniels <benjamindaniels21@gmail.com>
2024-01-01 11:18:17 -0500

    set marker to camp location

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
-  zoom: 7, // starting zoom
+  zoom: 9, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
